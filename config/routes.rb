8be84c57c11075fc39670361ab0ea3355191d0af
diarisdiakite <diarisdiakite@gmail.com>
2023-11-15 17:52:11 +0100

    Add basic application skeleton with Authentication and Authorization. Add linters.

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+﻿Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "/users/:id", to "users#show"
+  
+  resources :users do
+    resources :recipes , only: [:index, :new, :create, :show] do
+      member do
+        post 'like'
+      end
+    end
+    resources :foods , only: [:index, :new, :create, :show]
+  end
+  
+  # Defines the root path route ("/")
+  root "recipes#index"
+end
