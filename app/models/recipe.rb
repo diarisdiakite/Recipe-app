037e608694ab0b9c0abe92a2c5455071c6344cc6
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-11-17 15:56:47 +0430

    models added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,25 @@
-﻿class Recipe < ApplicationRecord
+class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+  # has_and_belongs_to_many :foods, join_table: 'recipe_foods'
   has_many :foods, through: :recipe_foods
+  scope :public_recipes, -> { where(public: true).order(created_at: :desc) }
 
-  validates :name, presence: true, length: { maximum: 250 }
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
 
-  scope :public_recipes, -> { where(public: true) }
-  scope :private_recipes, -> { where(public: false) }
+  def total_foods_price
+    prices = []
+
+    foods.each do |food|
+      recipe_food = RecipeFood.find_by(recipe_id: id, food_id: food.id)
+      food_total = (recipe_food.quantity * food.price)
+      puts '--------------------------------------'
+      puts "recipe: #{name}, food: #{food.name}, quantity: #{recipe_food.quantity}, price: #{food.price}"
+      prices.push(food_total)
+    end
+    prices.sum
+  end
 end
