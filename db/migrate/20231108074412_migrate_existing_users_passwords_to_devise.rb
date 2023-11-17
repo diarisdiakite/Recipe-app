1cf3864d96ab4833b7508e614d2055eb1d92701b
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-11-17 15:59:19 +0430

    Migrations added

--- db/migrate/20231108074412_migrate_existing_users_passwords_to_devise.rb
+++
@@ -1,9 +0,0 @@
-class MigrateExistingUsersPasswordsToDevise < ActiveRecord::Migration[7.0]
-  def up
-    User.find_each do |user|
-      new_password = SecureRandom.hex(8)
-      user.password = new_password
-      user.save
-    end
-  end
-end
